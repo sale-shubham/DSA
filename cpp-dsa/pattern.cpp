9747a9dca662681d412ba5264f77e0ac2cd71609
sale-shubham <saleshubham2121@gmail.com>
2026-04-19 13:40:33 +0000

    add initial implementation of simple pattern printing in C++

---
+++ cpp-dsa/pattern.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void simplepattern(int n)
+{
+    cout << "hello111111" << endl;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
+int main()
+{
+    int n = 5;
+
+    simplepattern(n);
+    return 0;
+}
