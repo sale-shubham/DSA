d94c0c84958c1eec310afc95e4d933ecf260b233
sale-shubham <saleshubham2121@gmail.com>
2026-04-19 16:09:01 +0000

    add multiple pattern functions and update main to include them

--- cpp-dsa/pattern.cpp
+++ cpp-dsa/pattern.cpp
@@ -15,10 +15,145 @@ void simplepattern(int n)
     }
 }
 
+void pattern2(int n)
+{
+    cout << "Pattern 2" << endl;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j <= i; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
+
+void pattern3(int n)
+{
+    cout << "Pattern 3" << endl;
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << j;
+        }
+        cout << endl;
+    }
+}
+
+void pattern4(int n)
+{
+    cout << "Pattern 2" << endl;
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << i << " ";
+        }
+        cout << endl;
+    }
+}
+
+void pattern5(int n)
+{
+    cout << "Pattern 5" << endl;
+
+    for (int i = 1; i <= n; i++) //   1
+    {                            // j= 1;  j<=4
+        for (int j = 1; j <= n - i + 1; j++)
+        {
+            cout << "*" << " ";
+        }
+        cout << endl;
+    }
+}
+
+void pattern6(int n)
+{
+    cout << "Pattern 6" << endl;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 1; j < n - i + 1; j++)
+        {
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+}
+
+//    *       4 space 1 star 4 space
+//   ***      3 space 3 star 3 space
+//  *****     2 space 5 star 2 space
+// *******    1 space 7 star 1 space
+//*********   0 space 9 star 0 space
+// row 5
+
+void pattern7(int n)
+{
+    cout << "Pattern 7" << endl;
+
+    for (int i = 0; i < n; i++)
+    {
+        // space
+        for (int j = 0; j < n - i; j++)
+        {
+            cout << " ";
+        }
+        // star
+        for (int j = 0; j < (2 * i) + 1; j++)
+        {
+            cout << i;
+        }
+        // space
+        for (int j = 0; j < n - i; j++)
+        {
+            cout << " ";
+        }
+        cout << endl;
+    }
+}
+
+void pattern8(int n)
+{
+    cout << "Pattern 8" << endl;
+
+    for (int i = 0; i < n; i++)
+    {
+        // space
+        for (int j = 0; j < i; j++)
+        {
+            cout << " ";
+        }
+        // star
+        for (int j = 0; j < ((n - i) * 2) - 1; j++)
+        {
+            cout << "*";
+        }
+        // space
+        for (int j = 0; j < i; j++)
+        {
+            cout << " ";
+        }
+        cout << endl;
+    }
+}
+
 int main()
 {
     int n = 5;
 
     simplepattern(n);
+    pattern2(n);
+    pattern3(n);
+    pattern4(n);
+    pattern5(n);
+    pattern6(n);
+    pattern7(n);
+    pattern8(n);
+
     return 0;
 }
